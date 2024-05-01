0ca0985987feb7d60c9eee1dc7ec7f25db859c77
MunibaP <munibapervez596@gmail.com>
2024-04-30 22:54:45 -0400

    Updated code in files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import './App.css'
-
 function App() {
   return (
     <>
