05db53aa4b6b9a674babae9295e4b9708816422a
MunibaP <munibapervez596@gmail.com>
2024-04-30 22:43:53 -0400

    fixed bug in the code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about" className="bg-light py-5 pb-0 min-vh-100">
       <div className="container">
         <div className="row align-items-center">
-          {/* Image */}
+
           <div className="col-md-6 mb-4 mb-md-0">
             <img
               src={codingImage}
@@ -15,7 +15,7 @@ const About = () => {
             />
           </div>
           
-          {/* Text */}
+
           <div className="col-md-6 px-4 py-3">
             <div>
               <h2 className="text-center text-md-left display-4 text-primary mb-4" style={{fontWeight: 'bold', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)'}}>About Me</h2>
@@ -41,48 +41,3 @@ const About = () => {
 }
 
 export default About;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function About() {
-//     return (
-//         <div className='content-container'>
-//             <h1 className='about-me-title'>About me</h1>
-//             {/* <img src={avatar} className="rounded-circle Avatar" alt="Circular Avatar"></img> */}
-//             <p>I am a junior fullstack web developer. My passion for coding has comes from my love of problem solving and design - the joy in that moment when I find a solution to something that is been bugging me, whether it is working on creating something from scratch such as arts and craft. All about creativity, collaboration, and learning new things.</p>
-//             <br></br>
-//             <p>Some skills I bring to the table:</p>
-//             <br></br>
-//             <ul className=" skillList text-center">
-                           
-//                 <li className="skill">JavaScript</li>
-//                 <li className="skill">ReactJS</li>
-//                 <li className="skill">HTML & CSS</li>
-//                 <li className="skill">Node</li>
-//                 <li className="skill">Express</li>
-//                 <li className="skill">SQL</li>
-//                 <li className="skill">Logo & Web Design</li>
-//             </ul>
-//             <p className="bio">I look forward to working with you!</p>
-//         </div>
-//     )
-// }
